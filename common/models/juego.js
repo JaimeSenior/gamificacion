091de27f0708f178830ab069c5f9afd2628d7f38
Jaime Izquierdo <1832498@alu.murciaeduca.es>
2018-10-22 08:13:13 +0200

    RemoteHookJaime

--- common/models/juego.js
+++ common/models/juego.js
@@ -1,17 +1,18 @@
 'use strict';
 
 module.exports = function(Juego) {
-  Review.beforeRemote('create', function(context, user, next) {
+  Juego.beforeRemote('create', function(context, user, next) {
     context.args.data.date = Date.now();
     context.args.data.publisherId = context.req.accessToken.userId;
     next();
   });
-};
+
 
     Juego.prototype.getNombre = function(callback) {
       var nombre = this.nombre;
       // TODO
       callback(null, nombre);
     };
+};
 
 
